25b96b77d705079fcaa0a91d11548af07793d4e0
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-07-12 00:16:47 -0400

    fix: add guards to Config

--- src/config.py
+++ src/config.py
@@ -1,21 +1,36 @@
-
 #!/usr/bin/env python3
 
 import os
 import argparse
+from collections import defaultdict
 
 HOME = os.path.expanduser("~")
 CONFIG_FILE = "%s/.config/convas/config" % HOME
+# curl "https://canvas.umd.umich.edu/api/v1/courses?per_page=60&enrollment_type=student&include[]=syllabus_body&include[]=total_scores&include[]=public_description&include[]=course_progress&include[]=sections&include[]=teachers&include[]=term&include[]=favorites" -H 'Authorization: Bearer 1058~9gDGGWUjqY4zlMilrAa0sWRA2h5ZFJ7NSXURcPvVAbWn5HiCEq4mFBTizU3FVRlM' | jq > a.json
 
 
 class Config:
     """Read and manage Canvas Token configurations"""
 
-    def __init__(self, token):
-        self.token = token
+    def __init__(self):
+        self.map = defaultdict(str)
 
     def get_token(self) -> str:
-        return self.token
+        if self.map["TOKEN"] == "":
+            print("API Token not found")
+            exit()
+        return self.map["TOKEN"]
 
-    def read_config(): 
-        pass
+    def get_domain(self) -> str:
+        if self.map["domain"] == "":
+            print("Domain not found")
+            exit()
+        if self.map["domain"][-1] != "/":
+            self.map["domain"].append("/")
+        return self.map["domain"]
+
+    def read_config(self, file) -> None:
+        for line in file:
+            if "=" in line:
+                key, value = line.strip().split("=")
+                self.map[key] = value
