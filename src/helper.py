f0f5f3c980a4888ad07e0ef6332460b96a5ca04f
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-07-12 00:17:24 -0400

    fix: fix minor bug with showing panels

--- src/helper.py
+++ src/helper.py
@@ -1,8 +1,9 @@
 import logging
 from curses import panel
-from curses import window
+from curses import window, doupdate
 from re import sub
 from html import unescape
+from time import sleep
 
 logging.basicConfig(level=logging.INFO, filename="log")
 Logger = logging.getLogger("samplelogger")
@@ -26,8 +27,15 @@ def show_panel_hide_on_keypress(a_panel, win: window):
     a_panel.top()
     a_panel.show()
     panel.update_panels()
-    if win.getch() != -1:
-        hide_panel(a_panel, win)
+    doupdate()
+    win.refresh()
+
+    while True:
+        if win.getch() != -1:
+            a_panel.bottom()
+            a_panel.hide()
+            panel.update_panels()
+            break
 
 
 def clean_up_html(content: str):
